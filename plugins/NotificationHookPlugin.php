611133ac2d5731cbfdff5520fe0e5b2358873b04
claudionhangapc <claudionhangapc@gmail.com>
2022-08-25 20:26:27 -0300

    added class to load notification hooks

---
+++ plugins/NotificationHookPlugin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Plugins;
+
+
+
+class NotificationHookPlugin
+{
+
+    public function loadsHooks(){
+        add_action('briefing_was_filled', [ $this, 'briefingWasFilled' ] , 2,2);
+    }
+
+    /*
+    *
+    */
+    public function briefingWasFilled($post_id,$user_id){
+        update_option( "Hook_opcao_nova_do_entrou_aqui", [ $post_id,$user_id]);
+    }
+
+
+}
